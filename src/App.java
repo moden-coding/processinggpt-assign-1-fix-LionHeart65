9d1d0600947dbb41d319ddc9421ee20ad489aea4
Ari Yellin-Levine <arilev65@gmail.com>
2024-09-24 08:43:03 -0400

    m

--- src/App.java
+++ src/App.java
@@ -1,7 +1,28 @@
-import processing.core.*;
+import processing.core.PApplet;
 
 public class App extends PApplet {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         PApplet.main("App");
+    }
+
+    public void settings() {
+        size(400, 400);
+
+    }
+
+    public void setup() {
+        fill(43);
+    }
+
+    public void draw() {
+        fill(223, 134, 167);
+        strokeWeight(1);
+        stroke(23);
+        rect(50, 50, 50, 34);
+        fill(234, 54, 94);
+        strokeWeight(3);
+        stroke(75);
+        ellipse(100, 233, 50, 50);
+        line(50, 50, 100, 233);
     }
 }
